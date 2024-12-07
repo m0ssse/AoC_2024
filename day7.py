b28df5230dbdcb0006424e6796f1fe64d5c9283d
m0ssse <69191580+m0ssse@users.noreply.github.com>
2024-12-07 10:46:24 +0200

    Refactored day7.py

--- day7.py
+++ day7.py
@@ -1,31 +1,25 @@
-def check1(i, curr_result, numbers, target):
+def check(i, curr_result, numbers, target, N):
     if i==len(numbers):
         return curr_result==target
-    num = numbers[i]
-    res1 = curr_result*num
-    res2 = curr_result+num
-    return check1(i+1, res1, numbers, target) or check1(i+1, res2, numbers, target)
-
-def check2(i, curr_result, numbers, target):
-    if i==len(numbers):
-        return curr_result==target
+    if curr_result>target:
+        return False
     num = numbers[i]
     res1 = curr_result*num
     res2 = curr_result+num
     res3 = int(f"{curr_result}{num}")
-    return check2(i+1, res1, numbers, target) or check2(i+1, res2, numbers, target) or check2(i+1, res3, numbers, target)
+    return any([check(i+1, res, numbers, target, N) for res in [res1, res2, res3][:N]])
 
-def solve(fname, checkerfun):
+def solve(fname, N):
     res = 0
     with open(fname) as file:
         for line in file:
             target, nums = line.split(":")
             target = int(target)
             nums = [int(x) for x in nums.split()]
-            if checkerfun(1, nums[0], nums, target):
+            if check(1, nums[0], nums, target, N):
                 res+=target
     print(res)
 
 fname = "day7_input.txt"
-solve(fname, check1)
-solve(fname, check2)
+solve(fname, 2)
+solve(fname, 3)
