5f020b4279b7e7122aec7605a6f4950b65295c83
m0ssse <69191580+m0ssse@users.noreply.github.com>
2024-12-18 12:49:20 +0200

    Update day18.py
    Changed part 2 to use union find instead of BFS

--- day18.py
+++ day18.py
@@ -4,7 +4,7 @@ def part1(bytes):
     for i in range(1024):
         x, y = bytes[i]
         grid[y][x] = "#"
-    bfs(grid)
+    print(bfs(grid))
 
 def bfs(grid):
     n, m = len(grid), len(grid[0])
@@ -15,25 +15,62 @@ def bfs(grid):
         i, j, steps = queue[k]
         visited.add((i, j))
         if i==n-1 and j==m-1:
-            #print(steps)
-            return True
+            return steps
         for ii, jj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
             if ii<0 or ii>=n or jj<0 or jj>=m or (ii, jj) in visited or grid[ii][jj]=="#":
                 continue
             queue.append((ii, jj, steps+1))
             visited.add((ii, jj))
         k+=1
-    return False
+    return 0
 
 def part2(bytes):
     n, m = 71, 71
     grid = [['.']*m for _ in range(n)]
-    i=0
-    while bfs(grid):
-        x, y = bytes[i]
+    parents = {}
+    compsize = {}
+    for x, y in bytes:
         grid[y][x]="#"
-        i+=1
-    print(x, y)
+    for i, row in enumerate(grid):
+        for j, char in enumerate(row):
+            if char==".":
+                parents[(i, j)]=(i, j)
+                compsize[(i, j)]=1
+                for ii, jj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                    if ii<0 or ii>=n or jj<0 or jj>=m or grid[ii][jj]=="#":
+                        continue
+                    if (ii, jj) not in parents:
+                        parents[(ii, jj)] = (ii, jj)
+                        compsize[(ii, jj)]=1
+                    merge(parents, (i, j), (ii, jj), compsize)
+    k=-1
+    while repres(parents, (0, 0))!=repres(parents, (n-1, m-1)):
+        j, i = bytes[k]
+        grid[i][j]="."
+        parents[(i, j)]=(i, j)
+        compsize[(i, j)]=1
+        for ii, jj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if ii<0 or ii>=n or jj<0 or jj>=m or grid[ii][jj]=="#":
+                continue
+            merge(parents, (i, j), (ii, jj), compsize)
+        k-=1
+    print(j, i)
+
+def merge(parents, a, b, compsize):
+    a, b = repres(parents, a), repres(parents, b)
+    if a==b:
+        return
+    if compsize[a]<compsize[b]:
+        a, b = b, a
+    parents[b]=a
+    compsize[a]+=compsize[b]
+
+def repres(parents, x):
+    if x not in parents:
+        parents[x] = x
+    while x!=parents[x]:
+        x = parents[x]
+    return x
 
 
 fname = "day18_input.txt"
